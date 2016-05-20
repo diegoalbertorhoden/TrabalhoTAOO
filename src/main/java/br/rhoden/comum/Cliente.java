62f7d0a2d09ee933a2b1910590c9f213f0bb3a41
RHODEN <RHODEN@univel.br>
2016-05-20 12:55:49 -0300

    Configurações de conexão com o banco

--- src/main/java/br/rhoden/comum/Cliente.java
+++ src/main/java/br/rhoden/comum/Cliente.java
@@ -1,5 +1,41 @@
 package br.rhoden.comum;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Cliente")
 public class Cliente {
+	@Id
+	@GeneratedValue
+	private Long id;
+	@Column(name="nome")
+	private String nome;
+	@Column(name="telefone")
+	private String telefone;
+	@Column(name="endereco")
+	private String endereco;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
 
 }
